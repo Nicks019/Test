9f3dc2d23385847d6a03f972877e1bcbeb118288
Nicks019 <101559903+Nicks019@users.noreply.github.com>
2022-07-26 12:28:08 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime, date 
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+st.image("compunnel.png",width=100)
+
+st.title('Stock Forecast App')
+
+Product = ('Category_19', 'Category_06', 'Category_05','Category_07','Category_28')
+Selected_Product = st.selectbox('Select dataset for prediction', Product)
+
+N_Month = st.text_input(" Input Forecast Months ")
+
+
+df = pd.read_csv(r"D:/Historical Product Demand.csv",parse_dates=['Date'])
+index = df[ df['Order_Demand'] <1000 ].index
+df.drop(index,inplace=True)
+df['Date'] = pd.to_datetime(df['Date'])
+df['Year'] = df['Date'].dt.year
+df['Month'] = df['Date'].dt.month
+index1 = df[df['Year'] == 2011 ].index
+df.drop(index1,inplace=True)
+index2 = df[df['Year'] == 2017].index
+df.drop(index2,inplace=True)
+df.drop(['Year','Month'],axis=1,inplace=True)
+df.dropna(axis=0, inplace=True)
+q1=df['Order_Demand'].quantile(0.25)
+q2=df['Order_Demand'].quantile(0.50)
+q3=df['Order_Demand'].quantile(0.75)
+iqr=q3-q1
+upper_limit=q3+1.5*iqr
+lower_limit=q1-1.5*iqr
+upper_limit,lower_limit
+def limit_imputer(value):
+    if value > upper_limit:
+        return upper_limit
+    if value < lower_limit:
+        a=a+1
+        return lower_limit
+    else:
+        return value
+df['Order_Demand']=df['Order_Demand'].apply(limit_imputer)
+li = ['Category_019','Category_006','Category_028','Category_005','Category_007']
+df19 = df[df.Product_Category==li[0]]
+df19= df19.groupby('Date')['Order_Demand'].count().reset_index()
+df19 = df19.set_index(['Date'])
+df19= df19['Order_Demand'].resample('MS').mean()
+df19 = df19.fillna(df19.bfill())
+df_19=df19.to_frame()
+from pylab import rcParams
+import statsmodels.api as sm
+decomposition = sm.tsa.seasonal_decompose(df_19, model='multiplicative')
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+import statsmodels.stats.diagnostic as diag
+from statsmodels.tsa.stattools import adfuller
+model=sm.tsa.statespace.SARIMAX(df19,order=(1,1,1),seasonal_order=(1,1,0,12))
+results=model.fit()
+N_Month
+pred = results.get_prediction(start=pd.to_datetime('2014-05-01'), dynamic=True)
+pred_ci = pred.conf_int()
+pred_uc = results.get_forecast(steps = N_Month)
+pred_ci = pred_uc.conf_int()
+ax = df19.plot(label='observed', figsize=(16, 8))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Order_Demand')
+plt.legend()
+plt.show()
+FORECAST_19 = results.forecast(steps = N_Month)
+FORECAST_19=FORECAST_19.to_frame()
+inventory_management_list_19 = FORECAST_19['predicted_mean'].tolist()
+inventory_management_list_19
+stock=0
+refill_list=[]
+balanced_stock=[]
+order_placed=[]
+extra_order_for_refill=[]
+# for i in  range(len(list1)):
+#     print(i)
+for x in inventory_management_list_19:
+    if stock<=(x*1.2):
+        #then placedOrder
+        Extra_order=(x*1.2)-stock
+        stock=stock+Extra_order
+        refill_list.append(stock)
+        stock=stock-x#balancedStock
+        balanced_stock.append(stock)
+        order_placed.append(x)
+        extra_order_for_refill.append(Extra_order)
+    else:
+        Extra_order=0
+        stock=stock+Extra_order
+        refill_list.append(stock)
+        stock=stock-x
+        balanced_stock.append(stock)
+        order_placed.append(x)
+        extra_order_for_refill.append(Extra_order)
+df = pd.DataFrame(list(zip(inventory_management_list_19,extra_order_for_refill,refill_list,order_placed,balanced_stock)), columns =['order_demand','Refill_0rder','refill_list','order','balanced'],index=FORECAST_19.index)
+df
+
